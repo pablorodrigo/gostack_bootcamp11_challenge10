f525299fb1ad82ad1d8233cae00d49e30f128538
pablosilva <pablo.silva.rodrigo@gmail.com>
2020-07-05 22:22:17 -0400

    [ReactJS][Challenge10][web] -  remove a food plate

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -79,6 +79,9 @@ const Dashboard: React.FC = () => {
 
   async function handleDeleteFood(id: number): Promise<void> {
     // TODO DELETE A FOOD PLATE FROM THE API
+    await api.delete(`/foods/${id}`);
+
+    setFoods(foods.filter(food => food.id !== id));
   }
 
   function toggleModal(): void {
